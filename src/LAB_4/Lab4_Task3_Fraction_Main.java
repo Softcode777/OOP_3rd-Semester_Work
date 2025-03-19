4c032e7fb766f541db08896509490e745edd53f1
Softcode777 <156195113+Softcode777@users.noreply.github.com>
2025-03-19 10:59:33 +0500

    commit 1

--- src/LAB_4/Lab4_Task3_Fraction_Main.java
+++ src/LAB_4/Lab4_Task3_Fraction_Main.java
@@ -4,10 +4,8 @@ public class Lab4_Task3_Fraction_Main {
         public static void main(String[] args) {
             Fraction f1 = new Fraction(7, 10);
             Fraction f2 = new Fraction(1, 2);
-
             f1.display();  // 2/4
             f2.display();  // 1/2
-
             System.out.println("Are fractions equal? " + f1.equals(f2));  // true
         }
     }
