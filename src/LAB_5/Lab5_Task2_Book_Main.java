4843c2f07a7bff5aa151c31be96599bc27b4973c
Softcode777 <156195113+Softcode777@users.noreply.github.com>
2025-03-23 10:57:46 +0500

    commit 1

--- src/LAB_5/Lab5_Task2_Book_Main.java
+++ src/LAB_5/Lab5_Task2_Book_Main.java
@@ -3,7 +3,7 @@ package LAB_5;
 public class Lab5_Task2_Book_Main {
     public static void main(String[] args) {
 
-        Address adrs1 = new Address("2","10","d.i.khan",29111);
+          Address adrs1 = new Address("2","10","d.i.khan",29111);
         Person author = new Person("lian",adrs1);
         Book book1 = new Book(author,"java book","Pearson");
         System.out.println("Old Adsress of author is:");
